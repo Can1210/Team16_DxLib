65c14206bfc845382a041ae54e634c6de05c72b3
佐藤幹 <->
2020-05-22 09:31:29 +0900

    enumの修正

--- Team16/Actor/Bulletes/ShotgunBullet.cpp
+++ Team16/Actor/Bulletes/ShotgunBullet.cpp
@@ -25,7 +25,7 @@ void ShotgunBullet::setBulletType()
 	case PLAYER:
 		b_mType = Type::PLAYER_BULLET;
 		break;
-	case SUB_PLAYER:
+	case SUB_PLAYER1:
 		b_mType = Type::PLAYER_BULLET;
 		break;
 	case ENEMY:
