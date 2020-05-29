2683fd15f359022b5f2aa8a957be68396c5d2cbf
shinjou_kun <shinjoubou03@gmail.com>
2020-05-29 10:24:45 +0900

    画像！

--- Team16/Actor/Bulletes/TrakingBullet.cpp
+++ Team16/Actor/Bulletes/TrakingBullet.cpp
@@ -100,8 +100,16 @@ void TrakingBullet::draw(Renderer * renderer, Renderer3D * renderer3D)
 	Vector2 a = b_mVelocity;
 	float angle = atan2(a.y, a.x)* 180.0f / DX_PI_F;
 	angle = -angle;
-
-	renderer3D->draw3DTexture("bullet1", Vector3(b_mPosittion.x, b_mPosittion.y, 0.0f), Vector2(0.0f, 0.0f), Vector2(12.0f, 16.0f), 32.0f, angle + 270.0f);
+	if (b_mType == Type::ENEMY_BULLET)
+	{
+		renderer3D->draw3DTexture("bullet1", Vector3(b_mPosittion.x, b_mPosittion.y, 0.0f), Vector2(0.0f, 0.0f), Vector2(12.0f, 16.0f), 32.0f, angle + 270.0f);
+		
+	}
+	else
+	{
+		renderer3D->draw3DTexture("misaill", Vector3(b_mPosittion.x, b_mPosittion.y, 0.0f), Vector2(0.0f, 0.0f), Vector2(64.0f, 64.0f), 64.0f, angle + 90.0f);
+	}
+		
 	//renderer->draw2D("bullet", Vector2(b_mPosittion.x, b_mPosittion.y), Vector2(0, 0), Vector2(64, 64), Vector2(32, 32), Vector2(1.5f, 1.5f), angle + 90, 255);
 }
 
--- Team16/Scene/LoadScene.cpp
+++ Team16/Scene/LoadScene.cpp
@@ -36,7 +36,7 @@ void LoadScene::initialize()
 	TextureLoad::getInstance().add("bullet5", "Resouce/Texture/Bullet05.png");
 	TextureLoad::getInstance().add("bullet6", "Resouce/Texture/Bullet06.png"); 
 	TextureLoad::getInstance().add("bossBullet", "Resouce/Texture/Bullet_S05.png");
-
+	TextureLoad::getInstance().add("misaill", "Resouce/Texture/misaille.png");
 	TextureLoad::getInstance().add("bullet_en2", "Resouce/Texture/Bullet_S02.png");
 	TextureLoad::getInstance().add("bullet_en3", "Resouce/Texture/Bullet_S03.png");
 	TextureLoad::getInstance().add("bullet_en4", "Resouce/Texture/Bullet_S04.png");
